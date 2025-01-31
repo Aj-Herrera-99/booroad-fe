7db64235f875467a403820fee1ced9612e1d87f7
DanielFerraiuolo98 <daniel.ferraiuolo.98@gmail.com>
2025-01-31 17:17:41 +0100

    add css classes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,30 @@
 function Footer() {
   return (
-    <div className="bg-green-300 min-h-[10vh]">Footer</div>
+    <footer className="min-h-[10vh]">
+      <div className="logo-footer">
+        <img src="" alt="logo" />
+      </div>
+      <div className="links">
+        <ul>
+          <a href="/"><li>Link</li></a>
+          <a href="/"><li>Link</li></a>
+          <a href="/"><li>Link</li></a>
+        </ul>
+        <ul>
+          <a href="/"><li>Link</li></a>
+          <a href="/"><li>Link</li></a>
+          <a href="/"><li>Link</li></a>
+        </ul>
+        <ul>
+          <a href="/"><li>Link</li></a>
+          <a href="/"><li>Link</li></a>
+          <a href="/"><li>Link</li></a>
+        </ul>
+      </div>
+      <div className="icons">
+
+      </div>
+    </footer>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,20 @@
 function Header() {
   return (
-    <header className="bg-red-300 fixed w-screen h-[10vh] ">Header</header>
-    
+    <header className="fixed w-screen h-[10vh] top-0 left-0">
+      <div className="logo-header">
+        <img src="" alt="logo" />
+      </div>
+      <div className="navbar">
+        <ul>
+          <a href="/"><li>Home</li></a>
+          <a href="/about"><li>About</li></a>
+          <a href="/contacts"><li>Contacts</li></a>
+          <input type="search" name="search" id="search" />
+          <button className="btn">cerca</button>
+        </ul>
+      </div>
+    </header>
+
   )
 }
 
