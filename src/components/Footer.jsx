f7220a134292cf9e48804736783dc6ec6ebcaf45
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-02-01 21:54:39 +0100

    ui graphics

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,9 @@ import { FaYoutube } from "react-icons/fa";
 
 function Footer() {
     return (
-        <footer className="min-h-[10vh] [&>*]:w-1/3">
-            <div className="logo-footer">
-                <h2>Booroad</h2>
-            </div>
-            <div className="links">
+        <footer className="min-h-[10vh] flex items-center gap-4 p-4 bg-blue-300">
+            <h2 className="text-3xl font-light">Logo</h2>
+            <div className="links flex gap-4">
                 <ul>
                     <a href="/">
                         <li>Link</li>
@@ -44,7 +42,7 @@ function Footer() {
                     </a>
                 </ul>
             </div>
-            <div className="icons">
+            <div className="icons [&>*]:text-2xl w-full flex justify-end gap-2">
                 <FaFacebook className="facebook" />
                 <FaInstagram className="instagram" />
                 <FaTwitter className="twitter" />
@@ -54,4 +52,4 @@ function Footer() {
     );
 }
 
-export default Footer
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,48 @@
-function Header() {
-  return (
-    <header className="fixed w-screen h-[10vh] top-0 left-0">
-      <div className="logo-header">
-        <h2>Booroad</h2>
-      </div>
-      <div className="navbar">
-        <ul>
-          <a href="/"><li>Home</li></a>
-          <a href="/about"><li>About</li></a>
-          <a href="/contacts"><li>Contacts</li></a>
-          <input type="search" name="search" id="search" />
-          <button className="btn">cerca</button>
-        </ul>
-      </div>
-    </header>
+import { useRef } from "react";
+import { FaMagnifyingGlass, FaXmark } from "react-icons/fa6";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { NavLink } from "react-router-dom";
 
-  )
+function Header() {
+    const windowRef = useRef(null);
+
+    const showWindowMenu = () => {
+        console.log("test");
+        windowRef.current.classList.remove("-translate-y-full");
+    };
+
+    return (
+        <header className="fixed w-screen h-[10vh] top-0 left-0 bg-blue-300 px-4 flex justify-between items-center">
+            <h2 className="text-3xl font-light tracking-wider">BooRoad</h2>
+
+            <GiHamburgerMenu className="text-xl" onClick={showWindowMenu} />
+            <WindowMenu windowRef={windowRef} />
+            {/* <div className="navbar"></div> */}
+        </header>
+    );
+}
+
+function WindowMenu({ windowRef }) {
+    const hideWindowMenu = () => {
+        windowRef.current.classList.add("-translate-y-full");
+    };
+    return (
+        <div
+            ref={windowRef}
+            className="fixed inset-0 h-fit bg-black/95 transition-all duration-300 -translate-y-full text-white flex flex-col px-4 py-6 "
+        >
+            <FaXmark onClick={hideWindowMenu} className=" text-2xl self-end" />
+            <div className="flex flex-col items-center gap-4 text-lg">
+                <form className="flex items-center gap-2">
+                    <input type="search" name="search" id="search" />
+                    <FaMagnifyingGlass className="text-xl" />
+                </form>
+                <NavLink href="/">Home</NavLink>
+                <NavLink href="/about">About</NavLink>
+                <NavLink href="/contacts">Contacts</NavLink>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
