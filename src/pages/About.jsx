fdcba9b00ec8d7c02d6c816c7ccd3a7ff634a102
Danilo-Mosca <danilo.mosca85@gmail.com>
2025-02-03 17:00:07 +0100

    Added content in About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,14 @@
 function About() {
   return (
-    <div>About</div>
+    <>
+      <div className="text-center p-3">
+        <h1 className="font-extrabold uppercase mb-3">Non siamo un tour operator o un'agenzia di viaggio: siamo una community di viaggiatori.</h1>
+        <p>In questa pagina non ti raccontiamo come funziona <strong>BooRoad</strong>: no, vogliamo farti capire cos'è BooRoad o, per meglio dire, chi è BooRoad, chi siamo noi.</p>
+      </div>
+      <div className="text-center my-5 bg-[#73CDC0] p-3 border-x-3 rounded-r-md  rounded-l-md ">
+        <p>BooRoad nasce per connettere persone, culture e storie attraverso quella è la nostra più grande passione: viaggiare. Per fare questo, creiamo gruppi di 15 persone e li spediamo dall'altra parte del mondo per vivere avventure incredibili. Alcuni affermano che BooRoad gli ha cambiato la vita, altri giurano che non partiranno mai più per un viaggio di gruppo – perché BooRoad non è per tutti, ma ehi, meglio provare e pentirsi, che non provare mai, no?</p>
+      </div>
+    </>
   )
 }
 
